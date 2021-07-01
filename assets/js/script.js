a3cc6ae534f6e5386b20f2dc39e8663833087491
Askar Rizvi <askarrizvi@askars-mbp.lan>
2021-07-01 11:29:52 -0400

    Implemented randome number API from random.org

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,34 +4,44 @@ var containerEl = document.querySelector(".container");
 function getMovie() {
     //var validMovie = false;
     var genreArray = [];
+    var randNum;
 
-    var randNum = Math.floor((Math.random() * 846565)) + 1;
-    console.log(randNum);
+    //var randNum = Math.floor((Math.random() * 846565)) + 1;
 
-    var apiUrl = "https://api.themoviedb.org/3/movie/" + randNum + "?api_key=917cef113b241cde7141b1d0182ae920";
 
-    fetch(apiUrl).then(function (response) {
-        if (response.ok) {
-            response.json().then(function (data) {
-                for (i = 0; i<data.genres.length; i++){
-                    genreArray.push(data.genres[i].name);
-                }
-                console.log(genreArray);
-                if (data.adult == true) {
-                    console.log("Adult movie");
-                    getMovie();
-                }
-                else if(genreArray.includes("Drama")){
-                    console.log(data);
-                }
-                else {
-                    getMovie();
-                }
-            });
-        } else {
-            getMovie();
-        }
+    randNumApi = "https://www.random.org/integers/?num=1&min=2&max=846565&col=1&base=10&format=plain&rnd=new";
+
+    fetch(randNumApi).then(function (randNumResponse) {
+        return randNumResponse.json();
+    }).then(function (randNumData) {
+        randNum = randNumData;
+        console.log(randNum);
+        var apiUrl = "https://api.themoviedb.org/3/movie/" + randNum + "?api_key=917cef113b241cde7141b1d0182ae920";
+        return fetch(apiUrl).then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    for (i = 0; i < data.genres.length; i++) {
+                        genreArray.push(data.genres[i].name);
+                    }
+                    console.log(genreArray);
+                    if (data.adult == true) {
+                        console.log("Adult movie");
+                        getMovie();
+                    }
+                    else if (genreArray.includes("Drama")) {
+                        console.log(data);
+                    }
+                    else {
+                        getMovie();
+                    }
+                });
+            } else {
+                getMovie();
+            }
+        });
     });
+
+
 }
 
 getMovie();
