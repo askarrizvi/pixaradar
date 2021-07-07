99d5d0c331957473e16d5fc4a405c8a7bbe214a1
Askar Rizvi <askarrizvi88@gmail.com>
2021-07-07 17:05:34 -0400

    Modified favourites to access localstorage and retrieve favourite movies

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const apiKey = "4cf2360";
+const apiKey = "917cef113b241cde7141b1d0182ae920";
 var containerEl = document.querySelector(".container");
 var buttonEl = document.getElementById("#save-answers");
 var userGenre = [];
@@ -20,14 +20,14 @@ function getMovie() {
     //var randNum = Math.floor((Math.random() * 846565)) + 1;
 
 
-    randNumApi = "https://www.random.org/integers/?num=1&min=2&max=846565&col=1&base=10&format=plain&rnd=new";
+    var randNumApi = "https://www.random.org/integers/?num=1&min=2&max=846565&col=1&base=10&format=plain&rnd=new";
 
     fetch(randNumApi).then(function (randNumResponse) {
         return randNumResponse.json();
     }).then(function (randNumData) {
         randNum = randNumData;
         console.log(randNum);
-        var apiUrl = "https://api.themoviedb.org/3/movie/" + randNum + "?api_key=917cef113b241cde7141b1d0182ae920";
+        var apiUrl = "https://api.themoviedb.org/3/movie/" + randNum + "?api_key="+apiKey;
         return fetch(apiUrl).then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
