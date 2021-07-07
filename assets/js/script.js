178a2d1379dfb634cbc0263cbf940c8ee1f5b575
Askar Rizvi <askarrizvi@askars-mbp.lan>
2021-07-07 01:46:56 -0400

    Added loading modal, and the main modal now has a link to the movie's IMDB page

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,10 @@ var foundMovie = false;
 
 function getMovie() {
     //var validMovie = false;
-   /* var loading = $('div');
-    loading.addClass("loading-text");
-    loading.text = "Loading";
-   // $(".loading-con").append(loading);*/
+    /* var loading = $('div');
+     loading.addClass("loading-text");
+     loading.text = "Loading";
+    // $(".loading-con").append(loading);*/
 
 
     var genreArray = [];
@@ -33,29 +33,33 @@ function getMovie() {
                     for (i = 0; i < data.genres.length; i++) {
                         genreArray.push(data.genres[i].name);
                     }
-                    console.log("GA: "+genreArray);
+                    console.log("GA: " + genreArray);
                     console.log(foundMovie);
                     if (data.adult == true) {
                         console.log("Adult movie");
                         getMovie();
-                    } 
-                        for (i = 0; i < userGenre.length; i++) {
-                            console.log("UGi: "+userGenre[i]);
-                            if (genreArray.includes(userGenre[i])&&(!foundMovie)) {
-                                console.log(data);
-                                foundMovie = true;
-                                var poster = data.poster_path;
-                                console.log(poster);
-                                //$(".loading-text").hide();
-                                $(".modal-content").append('<img id="theImg" src="https://image.tmdb.org/t/p/original/'+poster+'" />');
-                                break;
-                            }
+                    }
+                    for (i = 0; i < userGenre.length; i++) {
+                        console.log("UGi: " + userGenre[i]);
+                        if (genreArray.includes(userGenre[i]) && (!foundMovie)) {
+                            console.log(data);
+                            foundMovie = true;
+                            var movieTitle = data.title;
+                            var imdbId = data.imdb_id;
+                            var poster = data.poster_path;
+                            console.log(poster);
+                            //$(".loading-text").hide();
+                            $(".movie-link").append('<a href="https://www.imdb.com/title/' + imdbId + '">' + movieTitle + '</a>');
+                            $(".poster-container").append('<img id="theImg" src="https://image.tmdb.org/t/p/original/' + poster + '" />');
+                            modalState();
+                            break;
                         }
-                        if (!foundMovie){
-                            console.log("here");
-                            getMovie();
-                        }
-                    
+                    }
+                    if (!foundMovie) {
+                        console.log("here");
+                        getMovie();
+                    }
+
                 });
             } else {
                 getMovie();
@@ -91,7 +95,7 @@ $("#save-answers").on('click', '#card_open', function () {
     if (!userGenre.includes(getMoodGenre(mood))) {
         userGenre.push(getMoodGenre(mood));
     }
-    console.log("UG: "+userGenre);
+    console.log("UG: " + userGenre);
     //debugger
     getMovie();
 });
@@ -100,9 +104,26 @@ $("#save-answers").on('click', '#card_open', function () {
 const card_open = document.getElementById('card_open')
 const card_close = document.getElementById('card_close')
 const card_panel = document.getElementById('card_panel')
+const load_panel = document.getElementById('load_panel')
+
+function loadingModal() {
+    if (load_panel.classList.contains('hidden')) {
+        // Show modal
+        load_panel.classList.remove('hidden')
+        load_panel.classList.add('block')
+
+        // Delete button
+        card_open.classList.add('hidden')
+        card_open.classList.remove('block')
+
+        // Start animation open
+        card_panel.classList.add('card_open')
+    }
+}
 
 function modalState() {
-    if(card_panel.classList.contains('hidden')) {
+    if (card_panel.classList.contains('hidden')) {
+        console.log("opening modal");
         // Show modal
         card_panel.classList.remove('hidden')
         card_panel.classList.add('block')
@@ -113,6 +134,9 @@ function modalState() {
 
         // Start animation open
         card_panel.classList.add('card_open')
+        //Hide Loading
+        load_panel.classList.add('hidden')
+        load_panel.classList.remove('block')
     } else {
         // Delete modal
         card_panel.classList.add('hidden')
@@ -124,9 +148,11 @@ function modalState() {
 
         // Remove animation open
         card_panel.classList.remove('card_open')
+        $(".movie-link").empty();
+        $(".poster-container").empty();
     }
-};
+}
 
-card_open.addEventListener('click', modalState);
+card_open.addEventListener('click', loadingModal);
 card_close.addEventListener('click', modalState);
 
